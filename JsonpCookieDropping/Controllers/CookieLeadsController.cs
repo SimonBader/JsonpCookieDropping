75c835f338be61a1efc16e06dccc7a24cecf787a
Simon Bader <simon.bader@verivox.com>
2017-10-09 10:43:40 +0200

    Code cleanup

---
+++ JsonpCookieDropping/Controllers/CookieLeadsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web.Http;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace JsonpCookieDropping.Controllers
+{
+    public class CookieLeadsController : ApiController
+    {
+        [RequireHttps]
+        public HttpResponseMessage Get(string vxid, string div, string zip, string cons)
+        {
+            var name = "cookieKey";
+            var value = new {vxid, div, zip, cons};
+            var valueAsJson = JsonConvert.SerializeObject(value);
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            var cookie = new CookieHeaderValue(name, valueAsJson)
+            {
+                Expires = DateTimeOffset.Now.AddMonths(1),
+                Domain = Request.RequestUri.Host,
+                Path = "/"
+            };
+            response.Headers.AddCookies(new[] { cookie });
+            response.Content = new StringContent(string.Empty, Encoding.UTF8, "application/javascript");
+            return response;
+        }
+    }
+}
